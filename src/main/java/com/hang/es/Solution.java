b8b62ed7825c1f33e23b1a8d7cc8696cb83a72b1
杭朋洁 <596593218@qq.com>
2022-08-03 14:43:34 +0800

    second commit

--- src/main/java/com/hang/es/Solution.java
+++ src/main/java/com/hang/es/Solution.java
@@ -20,6 +20,7 @@ import java.util.Deque;
  */
 class Solution {
     public int maxLevelSum(TreeNode root) {
+        System.out.println("hello");
         Deque<TreeNode> queue = new ArrayDeque<>();
         queue.offer(root);
         int ans = 1;
